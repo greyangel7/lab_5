55a4ba50eeb051e2265639b047cc708dec372f17
greyangel7 <mr.piskunoff2017@yandex.ru>
2025-05-23 11:53:57 +0600

    change class FindPhone

--- src/main/java/phones/FindPhone.java
+++ src/main/java/phones/FindPhone.java
@@ -2,10 +2,18 @@ package phones;
 import java.awt.Color;
 import java.util.LinkedList;
 import java.util.List;
-
 public class FindPhone {
-    private List<Phone> data=new PhoneDB().getPhoneData();
-    
+    private List<Phone> data = new PhoneDB().getPhoneData();
+    public List<Phone> findByTag(Tag tag) {
+        List<Phone> result = new LinkedList<>();
+        for (Phone ph : data) {
+            if (tag.find(ph)) {
+                result.add(ph);
+            }
+        }
+        return result;
+    }
+
     public List<Phone> byColor(Color col) {
         List<Phone> find=new LinkedList<>();
         for(Phone ph : data)
@@ -27,14 +35,27 @@ public class FindPhone {
         return find;
     }
     
-    public List<Phone> byModelAndPriceLow(String mod, double price) {
-        List<Phone> find=new LinkedList<>();
-        for(Phone ph : data)
-            if (ph.getModel().equalsIgnoreCase(mod) &&
-                                ph.getPrice()<price)   find.add(ph);
-        return find;
+   public List<Phone> byModelAndPriceLowAndColor(String mod, double price, Color color) {
+        Tag modelTag = new ModelTag(mod);
+        Tag priceTag = new PriceLowTag(price);
+        Tag colorTag = new ColorTag(color);
+
+        Tag combinedTag = new AndTag(new AndTag(modelTag, priceTag), colorTag);
+
+        return findByTag(combinedTag);
     }
-    
+
+    public List<Phone> byModelAndPriceLow(String mod, double price) {
+        Tag modelTag = new ModelTag(mod);
+        Tag priceTag = new PriceLowTag(price);
+    //    Color color = null;
+    //    Tag colorTag = new ColorTag(color);
+
+        Tag combinedTag = new AndTag(modelTag, priceTag);
+
+        return findByTag(combinedTag);
+    }
+
     public List<Phone> byMSizeAndNotColor(int memSize, Color col) {
         List<Phone> find=new LinkedList<>();
         for(Phone ph : data)
